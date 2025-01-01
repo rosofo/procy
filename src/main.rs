4f1c2a356e418f6a0b8678b9f989b641ef547038
Robin Forbes <rsforbes0@gmail.com>
2025-01-01 11:40:16 +0000

    feat: add bevy_ecs_tilemap dependency and terrain plugin

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,26 @@ mod plugins;
 mod prelude;
 use bevy::log::{Level, LogPlugin};
 use iyes_perf_ui::prelude::PerfUiAllEntries;
-use plugins::caves::{caves_plugin, Caves};
+use plugins::{
+    caves::{caves_plugin, Caves},
+    terrain::terrain_plugin,
+};
 use prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(LogPlugin {
-            level: Level::DEBUG,
-            filter: "wgpu=error,bevy_render=info".to_string(),
+            level: Level::INFO,
+            filter: "wgpu=error,procy=debug".to_string(),
             custom_layer: |_| Some(Box::new(tracing_tracy::TracyLayer::default())),
         }))
         .add_plugins(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
         .add_plugins(bevy::diagnostic::EntityCountDiagnosticsPlugin)
-        .add_plugins(bevy::diagnostic::SystemInformationDiagnosticsPlugin)
         .add_plugins(iyes_perf_ui::PerfUiPlugin)
+        .add_plugins(TilemapPlugin)
         .add_plugins(Shape2dPlugin::new(ShapeConfig::default_2d()))
-        .add_plugins(caves_plugin)
+        // .add_plugins(caves_plugin)
+        .add_plugins(terrain_plugin)
         .add_systems(Startup, setup)
         .add_systems(FixedPostUpdate, || {
             tracy_client::secondary_frame_mark!("Fixed Frame");
@@ -29,7 +33,7 @@ fn main() {
 }
 
 fn setup(mut cmd: Commands) {
-    cmd.spawn((Camera2d, Msaa::Off));
+    cmd.spawn(Camera2d);
     cmd.spawn(Caves {
         size: Vec2::new(256.0, 256.0),
         ..default()
