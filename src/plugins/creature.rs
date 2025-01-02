54ec4c22666ee6ef9cddbb05fc6f670593617b7a
Robin Forbes <rsforbes0@gmail.com>
2025-01-02 10:50:25 +0000

    flap the bats

--- src/plugins/creature.rs
+++ src/plugins/creature.rs
@@ -2,14 +2,16 @@ use crate::prelude::*;
 
 pub fn creature_plugin(app: &mut App) {
     app.add_systems(Startup, setup);
+    app.add_systems(FixedUpdate, flap);
 }
 
 #[derive(Component)]
 #[require(
     Transform,
     RigidBody(|| RigidBody::Dynamic),
+    LockedAxes(|| LockedAxes::ROTATION_LOCKED_Z),
     Collider(|| Collider::ball(1.5)),
-    ColliderMassProperties(|| ColliderMassProperties::Density(1.2)),
+    ColliderMassProperties(|| ColliderMassProperties::Density(6.2)),
     Restitution(|| Restitution {coefficient: 0.7, ..default()}),
     GravityScale(|| GravityScale(1.5)),
 
@@ -32,15 +34,47 @@ fn setup(world: &mut World) {
             parent.spawn((
                 collider.clone(),
                 RigidBody::Dynamic,
+                ColliderMassProperties::Density(0.3),
+                GravityScale(1.0),
                 Transform::from_xyz(-15.0, 0.0, 0.0),
+                Ccd::enabled(),
+                Friction::coefficient(0.9),
+                Velocity::default(),
+                Sleeping::disabled(),
                 ImpulseJoint::new(parent.parent_entity(), joint_a),
             ));
             parent.spawn((
                 collider,
                 RigidBody::Dynamic,
+                Sleeping::disabled(),
+                GravityScale(1.0),
+                ColliderMassProperties::Density(0.3),
                 Transform::from_xyz(15.0, 0.0, 0.0),
+                Ccd::enabled(),
+                Velocity::default(),
+                Friction::coefficient(0.9),
                 ImpulseJoint::new(parent.parent_entity(), joint_b),
             ));
         });
     });
+}
+
+fn flap(
+    bats: Query<&Children, With<Bat>>,
+    mut wings: Query<&mut ImpulseJoint, With<Parent>>,
+    time: Res<Time>,
+) {
+    for children in bats.iter() {
+        let mut sign = 1.0;
+        let mut iter = wings.iter_many_mut(children);
+        while let Some(mut joint) = iter.fetch_next() {
+            let TypedJoint::RevoluteJoint(ref mut joint) = joint.data else {
+                continue;
+            };
+
+            let target_pos = (-0.6 + (time.elapsed_secs() * 5.0).sin()) * 0.5;
+            joint.set_motor_position(sign * target_pos, 2000.0, 30.0);
+            sign *= -1.0;
+        }
+    }
 }
